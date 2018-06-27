e7ba2e2d63ee871e03433c4858f5cf79c74327d6
CursedID <Cursed@163.com>
2018-06-27 13:31:42 +0800

    initial commit

---
+++ tou_sai_zi/scripts.js
@@ -0,0 +1,6 @@
+var DiceRollOne = Math.floor(Math.random() * 100) + 1;
+var DiceRollTwo = Math.floor(Math.random() * 100) + 1;
+alert('玩家1，想投色子，请点击确定.');
+document.write('玩家1投出了' + DiceRollOne + '点！' + '<br/>');
+alert('玩家2，想投色子，请点击确定.');
+document.write('玩家2投出了' + DiceRollTwo + '点！')
